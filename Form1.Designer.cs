bae28b48ab5b37c9a6565469e500f04643f8a33f
pmg2130 <eric2942@naver.com>
2026-04-30 15:00:41 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace SimplePaint
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            groupBox1 = new GroupBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("한컴 말랑말랑 Bold", 27.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 129);
+            label1.ForeColor = Color.Navy;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(233, 47);
+            label1.TabIndex = 0;
+            label1.Text = "Simple Paint";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Location = new Point(12, 82);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(386, 128);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(984, 661);
+            Controls.Add(groupBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private GroupBox groupBox1;
+    }
+}
